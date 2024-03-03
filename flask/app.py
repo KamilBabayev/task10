84b9ad5cb127f6c949b6acfd85421c201a3e5e23
Kamil Babayev <kamilyunis@gmail.com>
2024-03-03 17:06:53 +0400

    updated note json format to key value

--- flask/app.py
+++ flask/app.py
@@ -34,41 +34,29 @@ def get_note(note_id):
 @app.route('/api/v1/add_note', methods=['POST'])
 def add_note():
     data = request.get_json()
-
-    for k, v in data.items():
-        note_name = k
-        note_desc = v
-
+    
+    note_name = data['name']
+    note_desc = data['desc']
+    
     new_note = Note(name=note_name, desc=note_desc)
     db.session.add(new_note)
     db.session.commit()
-    return jsonify({'msg': f'user with {new_note.id} added successfully'})
+    return jsonify({'msg': f'note with id {new_note.id} added successfully'})
 
 
 @app.route('/api/v1/notes/<int:note_id>', methods=['PUT'])
 def update_note(note_id):
 
     note = Note.query.get(note_id)
-    # note = Note.query.filter_by(id=note_id).first()
-    print(note_id)
-    print(note)
 
     if note:
         data = request.get_json()   
-        print(data)
         if 'name' in data:
             note.name = data['name']
         if 'desc' in data:
             note.desc = data['desc']
         
         db.session.commit()
-
-        # if ''
-        # for k, v in data.items():
-        #     updated_note_name = k
-        #     updated_note_desc = v    
-        # print(data)
-        # print(updated_note_desc, updated_note_name)
     
         return jsonify({'message': 'Note  updated'}), 200
 
